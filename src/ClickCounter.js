50ef1f98842f3fe2ca2c2291f9eb62f30476d84a
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-09-14 18:03:32 +0500

    ClickCounter

---
+++ src/ClickCounter.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function ClickCounter() {
+  const [count, setCount] = useState(0);
+  const handleChange = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <h1 onClick={handleChange}>Click Me!</h1>
+      <h3>Clicked {count} Times</h3>
+    </>
+  );
+}
+export default ClickCounter;

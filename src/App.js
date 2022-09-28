d727cc1ab88026700f901478a21b27d678c75ca8
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-09-28 16:46:15 +0500

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "antd/dist/antd.css";
-import ComponentA from "./ComponentA";
-import { StudentConsumer, StudentProvider } from "./UserContext";
-import { FacultyProvider } from "./FacultyContext";
-import ComponentD from "./ComponentD";
+// import ComponentA from "./ComponentA";
+// import { StudentConsumer, StudentProvider } from "./UserContext";
+// import { FacultyProvider } from "./FacultyContext";
+// import ComponentD from "./ComponentD";
+import Counter from "./Counter";
 // import ClickCounter from "./ClickCounter";
 // import KeyPressCounter from "./KeyPressCounter";
 // import MouseHoverCounter from "./MouseHoverCounter";
@@ -10,16 +11,17 @@ import ComponentD from "./ComponentD";
 //import TestingComponent from "./TestingComponent";
 
 function App() {
-  const user = { name: "Abdul Waheed", age: 21, gender: "Male" };
-  const name = "Khalid Dheto";
+  // const user = { name: "Abdul Waheed", age: 21, gender: "Male" };
+  // const name = "Khalid Dheto";
   return (
     <div>
-      <StudentProvider value={user}>
+      <Counter />
+      {/* <StudentProvider value={user}>
         <FacultyProvider value={name}>
           <ComponentA />
           <ComponentD />
         </FacultyProvider>
-      </StudentProvider>
+      </StudentProvider> */}
       {/* <ComponentA /> */}
       {/* <SignupForm /> */}
       {/* <TestingComponent />

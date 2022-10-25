28ca709fd77fa6a2cfafd9cb33c998fe642ff377
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-10-25 22:45:21 +0500

    hide-unhide-classTask

--- src/ClassTask/TestComp.js
+++ src/ClassTask/TestComp.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TestComp({ objArray }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const expend = () => {
+    setIsVisible(!isVisible);
+  };
   return (
     <>
       {objArray.map((item) => {
         return (
           <div style={{ paddingLeft: 15 }}>
-            <input type={"checkbox"} />
-            <span>{item.name}</span>
-            {item.child ? <TestComp objArray={item.child} /> : <></>}
+            <span onClick={expend}>{item.name}</span>
+            {item.child ? (
+              isVisible ? (
+                <TestComp objArray={item.child} />
+              ) : (
+                <></>
+              )
+            ) : (
+              <></>
+            )}
           </div>
         );
       })}
